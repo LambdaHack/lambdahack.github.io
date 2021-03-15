b0342eb699893e70824dacb74acfb10500fa964e
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2021-03-15 17:49:56 +0100

    Create and add lz-string externs for minification

---
+++ lz-string.extern.js
@@ -0,0 +1,18 @@
+/**
+ * @const
+ */
+var LZString = {};
+
+/**
+ * @param {string} input
+ * @return {string}
+ */
+LZString.compressToUTF16;
+
+/**
+ * @param {string} compressed
+ * @return {string}
+ */
+LZString.decompressFromUTF16;
+
+module.exports = LZString;
